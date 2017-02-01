7a4ca9d1e2242a90276e684acef0179805783176
synion <lach.szymon86@gmail.com>
2017-02-01 14:29:32 +0100

    OO my solutiions

--- 74i.rb
+++ 74i.rb
@@ -1,19 +1,43 @@
-File.open('74.txt').each_line do |line|
-  number = line.to_i
-  i = 0
-  while number >= 1 do
-    if number >= 5
-      i += (number / 5)
-      number %= 5
-    elsif number >= 3
-      i += (number / 3)
-      number %= 3
-    else
-       i += (number / 1)
-       break
-    end  
+require 'pry'
+class Coin
+  def coins
+    @coins = [1,3,5]
+  end 
+end 
+
+class Values
+  attr_reader :data
+  
+  def initialize(data)
+    @data = data
   end
-  puts i
+
+  def data_to_number
+    @data = self.data.to_i
+  end
+
+  def minimum_of_coin
+    coin = Coin.new.coins
+    self.data_to_number
+    i = 0
+    while self.data >= 1 do
+      if self.data >= coin[2]
+        i += (self.data / coin[2])
+        @data %= coin[2]
+      elsif self.data >= coin[1]
+        i += (self.data / coin[1])
+        @data %= coin[1]
+      else
+         i += (self.data / coin[0])
+         break
+      end   
+    end  
+    return i
+  end
 end
 
-      
+File.open('74.txt').each_line do |line|
+  puts Values.new(line).minimum_of_coin
+end
+  
+ 
